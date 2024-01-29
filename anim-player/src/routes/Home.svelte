ec2e5a471307fc8bf68b5ada434f9e32ab218f7f
hansen1416 <badapplesweetie@gmail.com>
2024-01-29 22:59:48 +0800

    1

--- anim-player/src/routes/Home.svelte
+++ anim-player/src/routes/Home.svelte
@@ -3,7 +3,7 @@
 	import { onDestroy, onMount } from "svelte";
 	import * as THREE from "three";
 	import ThreeScene from "../lib/ThreeScene";
-	import { loadFBX, loadBVH } from "../utils/ropes";
+	import { loadFBX, loadJSON } from "../utils/ropes";
 	import * as SkeletonUtils from "three/addons/utils/SkeletonUtils.js";
 
 	/** @type {HTMLCanvasElement} */
@@ -47,82 +47,13 @@
 
 		Promise.all([
 			loadFBX(`/fbx/${model}.fbx`),
-			loadFBX(`/fbx-None-Keyframe-Reduction_2/${anim}.fbx`),
-			loadFBX(`/fbx-unity-t-pose-non-reduct/${anim}.fbx`),
-			loadBVH(`/bvh_t_pose/${anim}.bvh`),
-			loadFBX(`/mixamo-fbx/Aiming Gun.fbx`),
-		]).then(([fbx_model, fbx2, fbx1, bvh_data1, fbx3]) => {
+			loadJSON(`/anim-json/${anim}.json`),
+		]).then(([fbx_model, anim_data]) => {
 			anim_mixer = new THREE.AnimationMixer(fbx_model);
 			// console.log(fbx_unity_anim);
 			threeScene.scene.add(fbx_model);
 
-			const clip1 = fbx3.animations[0];
-
-			console.log(clip1.toJSON());
-
-			anim_action = anim_mixer.clipAction(clip1);
-			anim_action.reset();
-			anim_action.setLoop(THREE.LoopRepeat);
-			// keep model at the position where it stops
-			anim_action.clampWhenFinished = true;
-			anim_action.enabled = true;
-			// anim_action.fadeIn(0.5);
-			anim_action.play();
-
-			return;
-
-			console.log("fbx_model", fbx_model);
-
-			// fbx1.children.push(fbx_model.children[1]);
-			// fbx1.children.push(fbx_model.children[2]);
-
-			fbx1.children[0].traverse((child) => {
-				child.name = "mixamorig" + child.name;
-				// console.log(child.name);
-			});
-
-			fbx_model.children[0] = fbx_model.children[0];
-
-			console.log("fbx1", fbx1);
-
-			console.log("fbx2", fbx2);
-
-			anim_mixer = new THREE.AnimationMixer(fbx_model);
-			// console.log(fbx_unity_anim);
-			threeScene.scene.add(fbx_model);
-
-			const clip = fbx1.animations[0];
-			// const clip = fbx2.animations[0];
-
-			// console.log(clip);
-			// console.log(bvh_data1);
-			const tracks = [];
-			for (let i = 0; i < clip.tracks.length; i++) {
-				// console.log(bvh_data.clip.tracks[i]);
-				// if (clip.tracks[i].name.includes(".position")) {
-				// 	// tracks.push(bvh_data.clip.tracks[i]);
-				// } else {
-				clip.tracks[i].name = "mixamorig" + clip.tracks[i].name;
-				// const values = bvh_data.clip.tracks[i].values;
-				// for (let j = 0; j < values.length; j += 4) {
-				// 	// swap 0 and 2, 4 and 6, 8 and 10 and so on
-				// 	// if (j % 4 === 0) {
-				// 	const temp = values[j];
-				// 	values[j] = values[j + 2];
-				// 	values[j + 2] = temp;
-				// 	// }
-				// }
-				// bvh_data.clip.tracks[i].values = values;
-				tracks.push(clip.tracks[i]);
-				// }
-			}
-			// console.log(tracks);
-			clip.tracks = tracks;
-
-			// console.log(bvh_data.clip.toJSON());
-			// console.log(fbx_model.animations[0].toJSON());
-			// anim_action = anim_mixer.clipAction(bvh_data.clip);
-			// // anim_action = anim_mixer.clipAction(fbx_model.animations[0]);
+			const clip = THREE.AnimationClip.parse(anim_data);
 
 			anim_action = anim_mixer.clipAction(clip);
 			anim_action.reset();
