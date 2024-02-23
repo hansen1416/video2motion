fac33696df83fb9e9cb04335b558f1a7114bfa7b
Hanson <105476@grundfos.com>
2024-02-23 11:13:18 +0800

    1

--- anim-player/public/anim_euler.py
+++ anim-player/public/anim_euler.py
@@ -188,3 +188,87 @@ setFromRotationMatrix( m, order = this._order, update = true ) {
 
 }
 """
+
+
+class Vector3:
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+class Quaternion:
+    def __init__(self, x=0, y=0, z=0, w=1):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+
+
+class Matrix4:
+    def __init__(self):
+        self.elements = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+
+
+def matrix_rotation_from_quaternion(
+    position: Vector3, quaternion: Quaternion, scale: Vector3
+):
+    matrix = Matrix4()
+    te = matrix.elements
+
+    x = quaternion.x
+    y = quaternion.y
+    z = quaternion.z
+    w = quaternion.w
+
+    x2 = x + x
+    y2 = y + y
+    z2 = z + z
+
+    xx = x * x2
+    xy = x * y2
+    xz = x * z2
+
+    yy = y * y2
+    yz = y * z2
+    zz = z * z2
+
+    wx = w * x2
+    wy = w * y2
+    wz = w * z2
+
+    sx = scale.x
+    sy = scale.y
+    sz = scale.z
+
+    te[0] = (1 - (yy + zz)) * sx
+    te[1] = (xy + wz) * sx
+    te[2] = (xz - wy) * sx
+    te[3] = 0
+
+    te[4] = (xy - wz) * sy
+    te[5] = (1 - (xx + zz)) * sy
+    te[6] = (yz + wx) * sy
+    te[7] = 0
+
+    te[8] = (xz + wy) * sz
+    te[9] = (yz - wx) * sz
+    te[10] = (1 - (xx + yy)) * sz
+    te[11] = 0
+
+    te[12] = position.x
+    te[13] = position.y
+    te[14] = position.z
+    te[15] = 1
+
+    return matrix
+
+
+if __name__ == "__main__":
+    position = Vector3(0, 0, 0)
+    quaternion = Quaternion(0, 0, 0.7071068, 0.7071068)
+    scale = Vector3(1, 1, 1)
+
+    matrix = matrix_rotation_from_quaternion(position, quaternion, scale)
+
+    print(matrix.elements)
