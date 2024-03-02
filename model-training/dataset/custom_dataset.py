a621b7158a1cf63cf05500ef87effe1ae5e692b1
hansen1416 <badapplesweetie@gmail.com>
2024-03-02 18:20:28 +0800

    1

--- model-training/dataset/custom_dataset.py
+++ model-training/dataset/custom_dataset.py
@@ -1,9 +1,9 @@
 import os
-from typing import Tuple, Dict, List
+from typing import Tuple, Dict, List, Iterator, Sequence
 
 import numpy as np
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, Sampler
 
 
 class MediapipeDataset(Dataset):
@@ -86,19 +86,33 @@ class MediapipeDataset(Dataset):
         return self.current_inputs[idx], self.current_outputs[idx]
 
 
-if __name__ == "__main__":
+class FilewiseShuffleSampler(Sampler):
+    """
+    Shuffle the indices of the data in a filewise manner.
+    This is useful when the data is stored in multiple files and you want to shuffle the data within each file.
+    """
 
-    from torch.utils.data import DataLoader
-    from FilewiseShuffleSampler import FilewiseShuffleSampler
+    def __init__(
+        self, data_indices_in_files: Sequence[Sequence[int]] = None, generator=None
+    ) -> None:
+        """
+        Args:
+            data_indices_in_files (list): A list of list of indeices for each file.
+            generator (torch.Generator, optional): Generator used for shuffling. Default is None.
+        """
+        self.data_indices_in_files = data_indices_in_files
+        self.generator = generator
 
-    inputs_dir = os.path.join(os.path.dirname(__file__), "data", "inputs")
-    outputs_dir = os.path.join(os.path.dirname(__file__), "data", "outputs")
+    def __iter__(self) -> Iterator[int]:
+        for data_indices in self.data_indices_in_files:
+            for i in torch.randperm(len(data_indices), generator=self.generator):
+                yield data_indices[i]
 
-    if not os.path.exists(inputs_dir):
-        os.makedirs(inputs_dir)
+    def __len__(self) -> int:
+        return sum([len(sizes) for sizes in self.data_indices_in_files])
 
-    if not os.path.exists(outputs_dir):
-        os.makedirs(outputs_dir)
+
+def fetch_datatset_info(inputs_dir, outputs_dir) -> Tuple[Dict, List]:
 
     file_idx = 0
     indices_to_file_index = {}
@@ -118,6 +132,26 @@ if __name__ == "__main__":
 
         file_idx += 1
         accumulated_count += data.shape[0]
+
+    return indices_to_file_index, data_indices_in_files
+
+
+if __name__ == "__main__":
+
+    from torch.utils.data import DataLoader
+
+    inputs_dir = os.path.join(os.path.dirname(__file__), "data", "inputs")
+    outputs_dir = os.path.join(os.path.dirname(__file__), "data", "outputs")
+
+    if not os.path.exists(inputs_dir):
+        os.makedirs(inputs_dir)
+
+    if not os.path.exists(outputs_dir):
+        os.makedirs(outputs_dir)
+
+    indices_to_file_index, data_indices_in_files = fetch_datatset_info(
+        inputs_dir, outputs_dir
+    )
 
     print(f"indices_to_file_index size: {len(indices_to_file_index)}")
     print(f"data_indices_in_files: {data_indices_in_files}")
